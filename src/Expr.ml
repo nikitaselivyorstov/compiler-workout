5b4044f75a6c7614959c9cc38a2411d908e4fe13
Nikita <nikitaseliverstov@gmail.com>
2019-02-21 00:05:02 +0300

    Селиверстов Никита Максимович, СПБГЭТУ "ЛЭТИ", 4303

--- src/Expr.ml
+++ src/Expr.ml
@@ -3,10 +3,10 @@
 (* Opening a library for generic programming (https://github.com/dboulytchev/GT).
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
-open GT 
-             
-(* The type for the expression. Note, in regular OCaml there is no "@type..." 
-   notation, it came from GT. 
+open GT
+
+(* The type for the expression. Note, in regular OCaml there is no "@type..."
+   notation, it came from GT.
 *)
 @type expr =
   (* integer constant *) | Const of int
@@ -27,28 +27,51 @@ type state = string -> int
 (* Empty state: maps every variable into nothing. *)
 let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-(* Update: non-destructively "modifies" the state s by binding the variable x 
+(* Update: non-destructively "modifies" the state s by binding the variable x
    to value v and returns the new state.
 *)
 let update x v s = fun y -> if x = y then v else s y
 
-(* An example of a non-trivial state: *)                                                   
+(* An example of a non-trivial state: *)
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
 (* Some testing; comment this definition out when submitting the solution. *)
-let _ =
+(* let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
     ) ["x"; "a"; "y"; "z"; "t"; "b"]
-
+*)
 (* Expression evaluator
 
      val eval : state -> expr -> int
- 
-   Takes a state and an expression, and returns the value of the expression in 
+
+   Takes a state and an expression, and returns the value of the expression in
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+(* let eval = failwith "Not implemented yet" *)
+
+let intToBool x = x != 0
+
+let boolToInt y = if y then 1 else 0
+
+let calculate operation left right = match operation with
+  | "+" -> left + right
+  | "-" -> left - right
+  | "*" -> left * right
+  | "/" -> left / right
+  | "%" -> left mod right
+  | "<" -> boolToInt (left < right)
+  | ">" -> boolToInt (left > right)
+  | "<=" -> boolToInt (left <= right)
+  | ">=" -> boolToInt (left >= right)
+  | "==" -> boolToInt (left == right)
+  | "!=" -> boolToInt (left != right)
+  | "&&" -> boolToInt (intToBool left && intToBool right)
+  | "!!" -> boolToInt (intToBool left || intToBool right)
+
+  let rec eval st ex = match ex with
+  | Const constant -> constant
+  | Var variable -> st variable
+  | Binop(operation, left, right) -> calculate operation (eval st left) (eval st right)
